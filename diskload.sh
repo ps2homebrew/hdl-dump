b358b91cb62e7920e461afc3d4e675f5279e6106
Jessica Stokes <hello@jessicastokes.net>
2017-01-19 21:30:49 -0800

    Update diskload.sh to be less dangerous
    Previously, using diskload could fail catastrophically, destroying the hdl-dump working copy rather than deleting files in the open-ps2-loader directory.
    
    This updates it to;
    - Not delete all the unnecessary subdirectories of the checked out open-ps2-loader copy
    - Fail hard if the repository could not be cloned, or if the directory is missing somehow
    
    This makes it friendlier if you want to compile the server using Docker <https://github.com/mlafeldt/docker-ps2dev>

--- diskload.sh
+++ diskload.sh
@@ -1,31 +1,13 @@
 #!/bin/sh
 
+# Clone a fresh copy of Open PS2 Loader
 rm -rf open-ps2-loader
-hg clone https://bitbucket.org/ifcaro/open-ps2-loader
-cd open-ps2-loader/
-ls
-rm -rf elfldr
-rm -rf misc
-rm -rf gfx
-rm -rf labs
-rm -rf lng
-rm -rf pc
-rm -rf scripts
-rm -rf thirdparty
-rm -rf include
-rm -rf src
-rm -rf modules/cdvd
-rm -rf modules/debug
-rm -rf modules/isofs
-rm -rf modules/mcemu
-rm -rf modules/network
-rm -rf modules/ps2fs
-rm -rf modules/usb
-rm -rf modules/vmc
-rm -rf modules/wip
-rm -rf Makefile
-rm -rf .hg
+hg clone https://bitbucket.org/ifcaro/open-ps2-loader || exit 1
+cd open-ps2-loader/ || exit 1
+# Move the OPL readme across
 mv README README_OLD
+# Copy in custom stuff
 cp -rf ../diskload/* ./
+# Make it
 make clean
 make
