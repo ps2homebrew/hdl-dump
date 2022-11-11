4127df2ac054cea15edb48d299f527f78cc7fbc7
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-11-11 15:25:22 -0300

    [ISOFS]: remove unused variable `len_ext_ar`

--- isofs.c
+++ isofs.c
@@ -167,14 +167,13 @@ isofs_get_file_addr(iin_t *iin,
         u_int32_t start_addr, length;
         const unsigned char *p = buffer;
         do {
-            int len_dr, len_ext_ar;
+            int len_dr;
             u_int32_t name_len;
             const unsigned char *start = p;
             len_dr = *p++;
             if (len_dr == 0 ||
                 p > buffer + 2047)
                 break; /* last entry */
-            len_ext_ar = *p++;
             start_addr = ((u_int32_t)p[3] << 24 |
                           (u_int32_t)p[2] << 16 |
                           (u_int32_t)p[1] << 8 |
